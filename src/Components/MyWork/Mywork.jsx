05efb4d2b7a283514e0326c3591307a87e24fc05
shivanshu gupta <112546336+shiv-anshu-gupta@users.noreply.github.com>
2025-08-02 12:54:03 +0530

    Update Mywork.jsx

--- src/Components/MyWork/Mywork.jsx
+++ src/Components/MyWork/Mywork.jsx
@@ -3,44 +3,48 @@ import Work_data from "./Work_data";
 
 const Mywork = () => {
   return (
-    <div id="projects" className="text-white p-8">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl bg-gradient-to-r from-[#d299c2] to-[#fef9d7] bg-clip-text text-transparent p-2">
-          My Latest Work
-        </h1>
-      </div>
-
-      <div className="flex justify-center items-center">
-        <div className="w-9/12 flex justify-between m-10 flex-wrap">
-          <Work_data
-            Title="Client Project"
-            ImgProject="Images/client.png"
-            link="https://www.aditihdrealestate.com/"
-          />
-          <Work_data
-            Title="Hackathon Project"
-            ImgProject="Images/hackathonproject.png"
-            link="https://grievease-2-frontend.onrender.com/"
-          />
-          <Work_data
-            Title="Hotel Booking App"
-            ImgProject="Images/hotelBooking.png"
-            link="https://shivanshurecidency.onrender.com/"
-          />
-          <Work_data
-            Title="Elder Support"
-            ImgProject="Images/village.png"
-            link="https://oldcummunitysupport.onrender.com/"
-          />
-          <Work_data
-            Title="Grocery Store website"
-            ImgProject="Images/php.jpg"
-            link="https://github.com/shiv-anshu-gupta/GroceryStore"
-          />
+    <div
+      id="projects"
+      className="bg-gray-900 py-16 text-white dark:bg-gray-950 min-h-screen flex items-center justify-center"
+    >
+      <div className="container mx-auto px-4">
+        <div className="mb-12 flex flex-col items-center justify-center">
+          <h1 className="bg-gradient-to-r from-[#d299c2] to-[#fef9d7] bg-clip-text p-2 text-5xl font-bold text-transparent">
+            My Latest Work
+          </h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="grid w-full max-w-6xl grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <Work_data
+              Title="Client Project"
+              ImgProject="Images/client.png"
+              link="https://www.aditihdrealestate.com/"
+            />
+            <Work_data
+              Title="Hackathon Project"
+              ImgProject="Images/hackathonproject.png"
+              link="https://grievease-2-frontend.onrender.com/"
+            />
+            <Work_data
+              Title="Hotel Booking App"
+              ImgProject="Images/hotelBooking.png"
+              link="https://shivanshurecidency.onrender.com/"
+            />
+            <Work_data
+              Title="Elder Support"
+              ImgProject="Images/village.png"
+              link="https://oldcummunitysupport.onrender.com/"
+            />
+            <Work_data
+              Title="Grocery Store website"
+              ImgProject="Images/php.jpg"
+              link="https://github.com/shiv-anshu-gupta/GroceryStore"
+            />
+          </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Mywork;
+export default Mywork
